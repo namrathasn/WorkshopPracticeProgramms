9012ae546e6958decca29eca5ee4c010cb8b4814
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-01-29 23:17:37 +0530

    new

---
+++ ComparrableStudent.java
@@ -0,0 +1,41 @@
+package sortPractice;
+
+public class Student  implements Comparable<Student>{
+    private int id;
+    private String name;
+
+    @Override
+    public int compareTo(Student o) {
+        return this.id-o.id;
+    }
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+}
